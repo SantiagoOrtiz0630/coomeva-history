7c076c9368387923c25b8854f1c77102d653aac9
Sebastian Castaño Jara <sebastian.caja@gmail.com>
2020-02-24 18:05:37 -0500

    SCORM 1.2 API WORKS! Score works in Coomeva KME

--- js/main.js
+++ js/main.js
@@ -240,6 +240,11 @@ function clearVideoEnd(){
   endContainer.innerHTML = null;
 }
 
+function GetLMSScore(value){
+  let score = (value * SCORM_MAX_SCORE) / maxScore;
+  return parseInt(score);
+}
+
 //////RESULTS SYSTEM//////
 let resultsContainer = document.getElementsByClassName("App-results")[0];
 
@@ -294,40 +299,181 @@ function OnSlideChange() {
   }
 }
 
-//////KME360//////
+//////KME360, SCORM API//////
 
-let mainTitle = document.getElementsByClassName("App-title")[0];
+let scoreKme = 0;
 
-//
-let cc = "alejandro.dominguez";
-let token = "*$Km3Coomeva2018$*";
+//Include the standard ADL-supplied API discovery algorithm
 
-let str = md5(cc + "" + token).toString(16);
+///////////////////////////////////////////
+//Begin ADL-provided API discovery algorithm
+///////////////////////////////////////////
+var findAPITries = 0;
 
-let data = {
-  username: cc,
-  token: str
+function findAPI(win)
+{
+   // Check to see if the window (win) contains the API
+   // if the window (win) does not contain the API and
+   // the window (win) has a parent window and the parent window
+   // is not the same as the window (win)
+   while ( (win.API == null) &&
+           (win.parent != null) &&
+           (win.parent != win) )
+   {
+      // increment the number of findAPITries
+      findAPITries++;
+
+      // Note: 7 is an arbitrary number, but should be more than sufficient
+      if (findAPITries > 7)
+      {
+         alert("Error finding API -- too deeply nested.");
+         return null;
+      }
+
+      // set the variable that represents the window being
+      // being searched to be the parent of the current window
+      // then search for the API again
+      win = win.parent;
+   }
+   return win.API;
 }
 
-let miInit = {
-  mode: 'no-cors',
-  method: "POST",
-  body: JSON.stringify(data)
-};
+function getAPI()
+{
+   // start by looking for the API in the current window
+   var theAPI = findAPI(window);
 
-let strURL = `https://coomeva.kmelx.com/api/users/login`;
-
-function recibirDatos() {
-  fetch(strURL, miInit)
-    .then((response) => {
-      console.log(response);
-      return response.json();
-    }).then((myJson) => {
-      /*
-      console.log("DATOS-----------", myJson);
-      console.log("USER-----------", data);
-      */
-  });
+   // if the API is null (could not be found in the current window)
+   // and the current window has an opener window
+   if ( (theAPI == null) &&
+        (window.opener != null) &&
+        (typeof(window.opener) != "undefined") )
+   {
+      // try to find the API in the current window’s opener
+      theAPI = findAPI(window.opener);
+   }
+   // if the API has not been found
+   if (theAPI == null)
+   {
+      // Alert the user that the API Adapter could not be found
+      alert("Unable to find an API adapter");
+   }
+   return theAPI;
 }
 
-setTimeout(recibirDatos, 1000);
+function setValue(n, v) 
+{
+    var s = API.LMSSetValue(n, v);
+    return s;
+}
+
+function getValue(n)
+{
+    var s = API.LMSGetValue(n);
+    return s;
+}
+
+///////////////////////////////////////////
+//End ADL-provided API discovery algorithm
+///////////////////////////////////////////
+  
+//Create function handlers for the loading and unloading of the page
+
+//Constants
+var SCORM_TRUE = "true";
+var SCORM_FALSE = "false";
+var SCORM_MAX_SCORE = 100;
+var SCORM_MIN_SCORE = 0;
+
+//Since the Unload handler will be called twice, from both the onunload
+//and onbeforeunload events, ensure that we only call LMSFinish once.
+var finishCalled = false;
+
+//Track whether or not we successfully initialized.
+var initialized = false;
+
+var API = null;
+
+var initialTime = new Date();
+
+function ScormProcessInitialize(){
+    var result;
+    
+    API = getAPI();
+    
+    if (API == null){
+        alert("ERROR - Could not establish a connection with the LMS.\n\nYour results may not be recorded.");
+        return;
+    }
+    
+    result = API.LMSInitialize("");
+    
+    if (result == SCORM_FALSE){
+        var errorNumber = API.LMSGetLastError();
+        var errorString = API.LMSGetErrorString(errorNumber);
+        var diagnostic = API.LMSGetDiagnostic(errorNumber);
+        
+        var errorDescription = "Number: " + errorNumber + "\nDescription: " + errorString + "\nDiagnostic: " + diagnostic;
+        
+        alert("Error - Could not initialize communication with the LMS.\n\nYour results may not be recorded.\n\n" + errorDescription);
+        return;
+    }
+
+    console.log("Iniciado");
+    
+    initialized = true;
+}
+
+function ScormProcessFinish(){
+    
+    var result;
+
+    scoreKme = GetLMSScore(userScore);
+    
+    //var finalTime = new Date();
+
+   // var completedTimeSecs = ( finalTime.getTime() - initialTime.getTime() ) / 1000 ;
+
+    //Don't terminate if we haven't initialized or if we've already terminated
+    if (initialized == false || finishCalled == true){return;}
+    
+    if (typeof(scoreKme) === "number") {
+        setValue("cmi.core.score.raw", scoreKme);    
+    }
+
+    /*
+    if ( typeof(minCompletedTime === "number") && completedTimeSecs < minCompletedTime && getValue("cmi.core.lesson_status") !== "completed" ) {
+        setValue("cmi.core.lesson_status", "incomplete");
+    } else {
+        setValue("cmi.core.lesson_status", "completed");
+    }*/
+
+    setValue("cmi.core.lesson_status", "completed");
+    API.LMSCommit("");
+    
+    result = API.LMSFinish("");
+    
+    finishCalled = true;
+    
+    if (result == SCORM_FALSE){
+        var errorNumber = API.LMSGetLastError();
+        var errorString = API.LMSGetErrorString(errorNumber);
+        var diagnostic = API.LMSGetDiagnostic(errorNumber);
+        
+        var errorDescription = "Number: " + errorNumber + "\nDescription: " + errorString + "\nDiagnostic: " + diagnostic;
+        
+        alert("Error - Could not terminate communication with the LMS.\n\nYour results may not be recorded.\n\n" + errorDescription);
+        return;
+    }
+}
+
+/*
+Assign the processing functions to the page's load and unload
+events. The onbeforeunload event is included because it can be 
+more reliable than the onunload event and we want to make sure 
+that Terminate is ALWAYS called.
+*/
+
+window.onload = ScormProcessInitialize;
+window.onunload = ScormProcessFinish;
+window.onbeforeunload = ScormProcessFinish;
